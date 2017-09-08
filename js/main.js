c7a54301a73da8a9057e45840da3360601a51315
chobinrang <robinkchang@gmail.com>
2017-09-07 20:30:38 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is M.Reza", "I'm a Web Designer", "Love Simplicity"],
+        strings: ["My Name is Robin Chang", "I'm a software engineer", "Love Simplicity"],
         typeSpeed: 100,
         backDelay: 900,
         // loop
